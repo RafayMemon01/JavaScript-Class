e89fdc445e022b4aad6c79a1edfc6552154bdbd5
Rafay Memon <rafayrashid200611@gmail.com>
2024-01-11 19:36:28 +0500

    JavaScript Maths Expression

---
+++ 2. Javascript Varibles Name/script.js
@@ -0,0 +1,52 @@
+// let a = 1
+// let b = 2
+
+
+//don't recommended
+// let alert = 1
+
+// console.log(alert);
+
+
+// let name;
+
+// let myUserName;
+
+// let my_user_name;
+
+// a + b; // 3
+
+// let answer = a+b;
+
+// console.log("answer is", answer);
+
+// let a = 5;
+// let b = 10;
+
+// let answer = a+b;
+
+// let num = 10;
+// let popularNumber = num + 200;
+
+// console.log(popularNumber);
+
+let num1 = 1;
+
+// console.log(num1++);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+console.log(--num1);
+// let num2 = 3
+
+// console.log(num1++);
