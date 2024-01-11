ee40364d809ca03c914d5684143291ea62edfdbc
Rafay Memon <rafayrashid200611@gmail.com>
2024-01-11 14:42:24 +0500

    intoduction to javascript

---
+++ 1. Introduction to JavaScript and JS variable, alert and console/script.js
@@ -0,0 +1,32 @@
+// alert("Hello World!")
+
+// console.log("jhjhjkajh");
+
+// 3 data types
+
+// string 
+//number
+// boolean (true or false)
+
+// 2) Variables
+
+// we don't use var we use let and const
+
+let name;
+
+
+// let name = ""
+
+
+
+
+// name = "Muhammad"
+
+// const name = "Muhammad";
+
+// name = "owais"
+
+name= "muhammad"
+
+
+alert(name);
