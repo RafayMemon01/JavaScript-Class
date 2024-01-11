8628fb52d2f3d7fe38d440b39fa5ae611a4ecb8c
Rafay Memon <rafayrashid200611@gmail.com>
2024-01-11 14:45:31 +0500

    Some Changes to clear the questions

--- 1. Introduction to JavaScript and JS variable, alert and console/script.js
+++ 1. Introduction to JavaScript and JS variable, alert and console/script.js
@@ -1,32 +1,42 @@
-// alert("Hello World!")
+// Welcome to JavaScript Basics Documentation
 
-// console.log("jhjhjkajh");
+// 1) Displaying a simple alert
+// Alert function is used to display a pop-up message
+alert("Hello World!");
 
-// 3 data types
+// Logging a message to the console
+// Console.log is used to output messages to the browser console
+console.log("jhjhjkajh");
 
-// string 
-//number
-// boolean (true or false)
+// 2) Data Types
+// JavaScript has several data types. Three common ones are:
 
-// 2) Variables
+// a) String: A sequence of characters enclosed in single or double quotes
+let myString = "Hello, World!";
 
-// we don't use var we use let and const
+// b) Number: Numeric values, can be integers or floating-point numbers
+let myNumber = 42;
 
+// c) Boolean: Represents true or false values
+let isTrue = true;
+let isFalse = false;
+
+// 3) Variables
+// Variables are used to store and manage data.
+
+// a) Declaring a variable using 'let'
 let name;
 
+// b) Assigning a value to a variable
+name = "Muhammad";
 
-// let name = ""
+// c) Reassigning a value to the variable
+name = "Owais";
 
+// d) Using 'const' to declare a constant variable
+const companyName = "Example Corp";
 
+// Note: Once a value is assigned to a 'const' variable, it cannot be reassigned.
 
-
-// name = "Muhammad"
-
-// const name = "Muhammad";
-
-// name = "owais"
-
-name= "muhammad"
-
-
+// Displaying the value of the 'name' variable using an alert
 alert(name);
